538fcffcca6f16f265bac05f31a6a4fb329dce1c
Nico_le <156796855+NicoleMaini@users.noreply.github.com>
2024-02-02 10:43:16 +0100

    change

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,27 @@
 
 
 // bottone per estrarre i numeri
-const generateRandomNumber = () =>{
-    const btnEnter = document.querySelector("button")
-    const btnAndRandomNumberContainer = document.querySelector(".push-number")
-    console.log(btnAndRandomNumberContainer);
+
+const btnEnter = document.querySelector("button")
+const numberContainer = document.querySelector(".push-number h2")
+const arr = [];
+
 
     btnEnter.onclick = () => {
-        let randomNumber = document.createElement("h2")
-        randomNumber.innerText = Math.floor(Math.random()*100) + 1
+        
+        const randomNumber = Math.floor(Math.random()*arr.length)
         console.log(randomNumber);
 
-        btnAndRandomNumberContainer.appendChild(randomNumber)  
+        numberContainer.innerText = randomNumber 
         
-        const cellNumber = document.querySelector(".cell-container")
+        const cellNumber = document.getElementById(randomNumber)
 
         console.dir(cellNumber);
+        cellNumber.classList.add("check")
 
-        if(randomNumber.innerText === cellNumber.innerText){
-            cellNumber.classList.add("check")
-        }
+       
+
     }
-
-}
 
 // generiamo la funzione madre
 
@@ -30,28 +29,25 @@ const generateBoard = () =>{
     // generiamo l'elemento in cui ci inseriremo
     const board = document.querySelector(".container")
     console.log(board);
-
     
-    for(let i = 0; i < 99; i++){
+    for(let i = 0; i < 76; i++){
+        
+        arr.push(i + 1);
         
         const cellDiv = document.createElement("div") 
         cellDiv.classList.add("cell-container")
         
         const cell = document.createElement("h3") 
-        cell.innerText = i + 1
+        cell.innerText = i + 1;
+        cell.setAttribute("id", i + 1) 
         
         cellDiv.appendChild(cell)
         board.appendChild(cellDiv)
+
         
     }
     
-    generateRandomNumber()
 }
-
-
-
-
-
 
 window.onload = () =>{
 
